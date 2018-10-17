82c392c382519956dcf300a7c7cb4f5519dc16e3
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-17 20:18:59 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,11 +4,11 @@ require "pry"
 def load_library(file)
   text = YAML.load_file(file)
   
-  hash = {"get_emoticon"=>{"english_emoticon"=> {}}, "get_meaning"=>{"japanese_emoticon" => {}}}
+  hash = {"get_emoticon"=>{ {}}, "get_meaning"=>{ {}}}
   
   text.each do |meaning, array|
-    hash["get_emoticon"]["english_emoticon"][array[0]] = array[1]
-    hash["get_meaning"]["japanese_emoticon"][array[1]] = meaning
+    hash["get_emoticon"][array[0]] = array[1]
+    hash["get_meaning"][array[1]] = meaning
   end
 
    hash
@@ -16,8 +16,8 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   hash = load_library(file)
-  if hash["get_emoticon"]["english_emoticon"][emoticon]
-    return hash["get_emoticon"]["english_emoticon"][emoticon]
+  if hash["get_emoticon"][emoticon]
+    return hash["get_emoticon"][emoticon]
   else
     return "Sorry, that emoticon was not found"
   end
@@ -26,8 +26,8 @@ end
 def get_english_meaning(file, emoticon)
   hash = load_library(file)
   
-  if hash["get_meaning"]["japanese_emoticon"][emoticon]
-    return hash["get_meaning"]["japanese_emoticon"][emoticon]
+  if hash["get_meaning"][emoticon]
+    return hash["get_meaning"][emoticon]
   else
     return "Sorry, that emoticon was not found"
   end
